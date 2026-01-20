43a1bd2856835f1408bd157ce77800edac265990
Laura NogBap <nogueirabaptistalaura@gmail.com>
2026-01-20 11:49:15 -0300

    logic corrections

--- index.js
+++ index.js
@@ -17,21 +17,26 @@ class Subject{
         this.difficulty = difficulty
         Subject.all_instances.push(this)
         console.log(`Subject ${this.name} with difficulty ${this.difficulty} created.`)
+        console.log(Subject.all_instances)
     }
 }
 
 function Submit() {
     let name = document.getElementById('name').value
     let dif = parseInt(document.getElementById('dif').value)
-    new Subject(name, dif)
-    let listItem = document.createElement('div')
-    listItem.className = 'subject-card'
-    listItem.innerHTML = `
-    <span><strong>${name}</strong> (Difficulty: ${dif}) </span>
-    <button onclick='deleteSubject(this, "${name}")' class='btn-delete'>Delete</button>
-    `
-    let listSub = document.getElementById('listSub')
-    listSub.appendChild(listItem)
+    if (!name){
+        console.log('ERROR! Please insert the name of the subject.')
+    } else {
+        new Subject(name, dif)
+        let listItem = document.createElement('div')
+        listItem.className = 'subject-card'
+        listItem.innerHTML = `
+        <span><strong>${name}</strong> (Difficulty: ${dif}) </span>
+        <button onclick='deleteSubject(this, "${name}")' class='btn-delete'>Delete</button>
+        `
+        let listSub = document.getElementById('listSub')
+        listSub.appendChild(listItem)
+    }
 }
 
 function deleteSubject(elementBtn, name) {
@@ -46,61 +51,71 @@ function generatePlan(){
     let end = document.getElementById('end').value
     let bgn = document.getElementById('bgn').value
 
-    const [startHours, startMinutes] = bgn.split(':').map(Number);
-    const [endHours, endMinutes] = end.split(':').map(Number);
-
-    // 3. Normalização para Minutos
-    let totalStart = ((startHours * 60) + startMinutes)/60
-    let totalEnd = ((endHours * 60) + endMinutes)/60
-
-    if (totalStart>totalEnd){
-        totalEnd += 24
-    }
-
-    time = totalEnd-totalStart
-    Subject.all_instances.forEach(element => {
-        a += element.difficulty
-    });
-
-    factor = a/time
-
-    console.log('Your Planner')
-
-    startStampH = startHours
-    startStampM = startMinutes
-    let list = document.getElementById('plan')
-    list.replaceChildren()
-    Subject.all_instances.forEach(element => {
-        let hours = element.difficulty/factor
-        let minutes = hours*60%60
-        hours = parseInt(hours.toFixed(0))
-        minutes = parseInt((minutes*60%60).toFixed(0))
-        if (minutes < 1) {
-            console.log(`${element.name}: ${hours} hour(s).`)
-        } else {
-            console.log(`${element.name}: ${hours} hour(s) and ${minutes} minute(s).`)
+    if (!end || !bgn || Subject.all_instances.length == 0){
+        console.log('ERROR! Please, verify if you have put all the neccessary information.')
+    } else {
+        const [startHours, startMinutes] = bgn.split(':').map(Number);
+        const [endHours, endMinutes] = end.split(':').map(Number);
+    
+        //minutes
+        let totalStart = ((startHours * 60) + startMinutes)/60
+        let totalEnd = ((endHours * 60) + endMinutes)/60
+    
+        if (totalStart>totalEnd){
+            totalEnd += 24
         }
-        item = document.createElement('div')
-        item.className = 'subject-card'
-        item.style.borderLeft = `5px solid ${generateHex()}`
         
-        endStampH = startStampH + hours
-        endStampM = startStampM + minutes
-        
-        if (endStampH>=24){
-            endStampH -= 24
-        } else if (endStampM>=60){
-            endStampM -= 60
-        } 
-        
-        
-        item.innerHTML = `
-        <span><strong>${element.name}</strong></span>
-        <span>${String(startStampH).padStart(2, '0')}:${String(startStampM).padStart(2, '0')}-${String(endStampH).padStart(2, '0')}:${String(endStampM).padStart(2, '0')}</span>
-        `
-        list.appendChild(item)
+        //duration of the study session
+        time = totalEnd-totalStart
+        console.log(time)
+        Subject.all_instances.forEach(element => {
+            a += element.difficulty
+        });
+        //weighted average
+        factor = a/time
+    
+        console.log("Duration of each subjects's studying period:")
+    
+        startStampH = startHours
+        startStampM = startMinutes
+        let list = document.getElementById('plan')
+        list.replaceChildren()
+        Subject.all_instances.forEach(element => {
+            let hours = element.difficulty/factor
+            console.log(hours)
+            // the rest of the division by 60 = number of minutes
+            let minutes = hours*60%60
+            hours = parseInt(hours)
+            minutes = parseInt(minutes)
+            if (minutes == 0) {
+                console.log(`${element.name}: ${hours} hour(s).`)
+            } else if (hours == 0) {
+                console.log(`${element.name}: ${minutes} minute(s).`)
+            } else {
+                console.log(`${element.name}: ${hours} hour(s) and ${minutes} minute(s).`)
+            }
+            item = document.createElement('div')
+            item.className = 'subject-card'
+            item.style.borderLeft = `5px solid ${generateHex()}`
+            
+            endStampH = startStampH + hours
+            endStampM = startStampM + minutes
 
-        startStampH = endStampH
-        startStampM = endStampM
-    });
+            if (endStampM>=60){
+                endStampM -= 60
+                endStampH +=1
+            } if (endStampH>=24){
+                endStampH -= 24
+            }
+            
+            item.innerHTML = `
+            <span><strong>${element.name}</strong></span>
+            <span>${String(startStampH).padStart(2, '0')}:${String(startStampM).padStart(2, '0')}-${String(endStampH).padStart(2, '0')}:${String(endStampM).padStart(2, '0')}</span>
+            `
+            list.appendChild(item)
+    
+            startStampH = endStampH
+            startStampM = endStampM
+        });
+    }
 }
